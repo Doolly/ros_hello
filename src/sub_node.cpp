6aee7adc3afe43bc490bb99591821467463fa72e
yundoo97 <yundoo_97@naver.com>
2018-08-21 00:42:55 +0900

    dd

---
+++ src/sub_node.cpp
@@ -0,0 +1,63 @@
+#include "ros/ros.h"
+#include "ros_hello/Message1.h"
+#include "ros_hello/Message2.h"
+#include <iostream>
+#include "wiringPi.h"
+
+#define LED 1
+#define MOTOR 4
+
+int moter_val;
+
+void LedCallback(const ros_hello::Message2::ConstPtr& msg)
+{	
+	if(msg->ltemp == 1){
+		digitalWrite(LED,HIGH);
+		ROS_INFO("LED ON");
+		
+	}
+	if(msg->ltemp == 0){
+		digitalWrite(LED,LOW);
+		ROS_INFO("LED OFF");
+	}
+
+}
+
+
+void MotorCallback(const ros_hello::Message1::ConstPtr& msg)
+{	
+	moter_val = msg->mtemp;
+	ROS_INFO("MOTOR status= %d", moter_val);
+}
+
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "sub_node"); //node name
+	
+	wiringPiSetup();
+	pinMode(LED, OUTPUT);
+	pinMode(MOTOR, PWM_OUTPUT);
+	pwmSetClock(192);
+	pwmSetMode(PWM_MODE_MS);
+	pwmSetRange(2000);
+	pwmWrite(MOTOR,1500);
+
+	ros::NodeHandle nh;
+	ros::Subscriber ros_hello_sub1 = nh.subscribe("led_topic",10,LedCallback);
+	ros::Subscriber ros_hello_sub2 = nh.subscribe("motor_topic",10,MotorCallback);
+
+	ros::spin();
+	ros::Rate rate(10);
+	
+while(ros::ok){
+	pwmWrite(MOTOR,moter_val);
+
+	
+}
+return 0;
+}
+
+
+
